0e50c76dc5ec47fea0549060c31dea9681000760
lokeshbodade <lokeshbodade4@gmail.com>
2024-01-16 19:23:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('btn').addEventListener('click', function(){
+    alert("Congratulations..!!For Joining AlifeFitness Our Team Will Contact You")
+    })
+    document.getElementById('bttn').addEventListener('click', function(){
+        alert("Thanks For Joining Us..!!")
+        })
